c09da879a9f53703d55fff6d4088f4f1a6167747
Bridgette Drummond <BeeDrummond@bridgettes-air.dhcp.nd.edu>
2017-09-19 16:23:03 -0400

    final version

--- question3.sh
+++ question3.sh
@@ -1,10 +1,10 @@
 #question 3 - determining effect of graduating college
 
 echo college grad minimum wage
-cat "$1" | tr "," " " | cut -d" " -f3,4 | grep "16 " | sort -k2n | head -1 | cut -d" " -f2
+val1= cat "$1" | tr "," " " | cut -d" " -f3,4 | grep "16 " | sort -k2n | head -1 | cut -d" " -f2 
 
 echo high school grad minimum wage
-cat "$1" | tr "," " " | cut -d" " -f3,4 | grep "12 " | sort -k2n | head -1 | cut -d" " -f2
+val2= cat "$1" | tr "," " " | cut -d" " -f3,4 | grep "12 " | sort -k2n | head -1 | cut -d" " -f2
 
 
 
