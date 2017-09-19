26ba744728201ea440eae37b59a0e02fee6f7077
Brooke <weaverbe9@gmail.com>
2017-09-18 21:14:17 -0400

    question 2 completed I hope

--- question2.sh
+++ question2.sh
@@ -1,5 +1,5 @@
 # question 2 excercise 4
-# $1 is the file name
+# $1 is the file name that you are working on
 
 echo highest earner
 cat "$1" | tr "," " " | cut -d" " -f1,2,4 | sort -k3n | tail -1
